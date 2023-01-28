6a8c23c6c0cb5cabf4146fa4bc4847775df7ccd1
adjsky <igorlfmartins@mail.ru>
2023-01-28 22:22:58 +0300

    add todos lib logic, setup console stuff

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,47 @@
-pub fn print_hello_world() {
-    println!("Hello World!")
+use nanoid::nanoid;
+
+#[derive(Debug, Clone)]
+pub struct Todo {
+    pub id: String,
+    pub done: bool,
+    pub description: String,
+}
+
+pub struct Todos {
+    todos: Vec<Todo>,
+}
+
+impl Todos {
+    pub fn new() -> Todos {
+        Todos { todos: vec![] }
+    }
+
+    pub fn add(&mut self, description: &str) {
+        self.todos.push(Todo {
+            id: nanoid!(),
+            done: false,
+            description: String::from(description),
+        })
+    }
+
+    pub fn mark_as_done(&mut self, id: &str) {
+        self.todos.iter().position(|todo| todo.id == id);
+    }
+
+    pub fn remove(&mut self, id: &str) {
+        self.todos.retain(|todo| todo.id == id)
+    }
+}
+
+pub mod utils {
+    use std::io;
+
+    pub fn read_line() -> Option<String> {
+        let mut buffer = String::new();
+
+        match io::stdin().read_line(&mut buffer) {
+            Ok(_) => Some(buffer),
+            Err(_) => None,
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,37 @@
-use todo_rust::print_hello_world;
+use console::{style, Term};
+use dialoguer::{theme::ColorfulTheme, Select};
+use todo_rust::utils::read_line;
+use todo_rust::Todos;
 
-fn main() {
-    print_hello_world()
+fn main() -> std::io::Result<()> {
+    let term = Term::stdout();
+    Todos::new();
+
+    loop {
+        let line = read_line().unwrap();
+
+        match line.trim_end() {
+            "colored" => {
+                term.write_line(format!("{}", style("LOH").cyan()).as_str())?;
+            }
+            "select" => {
+                let items = vec!["Item 1", "item 2"];
+                let selection = Select::with_theme(&ColorfulTheme::default())
+                    .items(&items)
+                    .default(0)
+                    .interact_on_opt(&Term::stderr())?;
+
+                match selection {
+                    Some(index) => println!("User selected item : {}", items[index]),
+                    None => println!("User did not select anything"),
+                }
+            }
+            _ => {
+                println!("break");
+                break;
+            }
+        }
+    }
+
+    Ok(())
 }
